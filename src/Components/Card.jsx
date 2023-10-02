74f964b0b284ac110b10c9f35dbda2a1029834b3
sshivahh <russelshivah16@gmail.com>
2023-10-02 16:21:00 +0700

    add card, install babbel

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import styles from "./../Styles/Card.module.scss"
+
+function Card(){
+    return(
+        <div className={styles.card}>
+            
+        </div>
+    )
+}
+
+export default Card;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
+import styles from './../Styles/Home.module.scss'
 import Navbar from './../Components/Navbar';
 import Logo from './../Components/Logo'
-import styles from './../Styles/Home.module.scss'
+import Card from './../Components/Card'
 
 export default function Home(){
     return(
@@ -8,6 +9,11 @@ export default function Home(){
             <Navbar />
             <div className={styles.container}>
                 <Logo />
+                <div className={styles.cardContainer}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
             </div>
         </>
     )
