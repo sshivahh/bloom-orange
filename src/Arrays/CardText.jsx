4fc9b307b934fa5b4fe2f8a188d68dc873aade7a
sshivahh <russelshivah16@gmail.com>
2023-10-04 09:53:12 +0700

    render 10 text random setiap refresh

--- src/Arrays/CardText.jsx
+++ src/Arrays/CardText.jsx
@@ -1,9 +1,44 @@
+import Card from "../Components/Card"
+
+
+
 const CardText = [
         "halo olivia",
         "hai apakabar olivia",
         "hello olivia",
         "konichiwa orivia",
-        "ni hao cina"
-    ]
+        "ni hao cina",
+        "halo woi",
+        "apakabar oliv",
+        "haiiii woi",
+        "Olivi djohan",
+        "makan bubur yuk",
+        "laper",
+        "HIIII",
+        "main emel",
+        "mau jalan ga"
+]
 
-export default CardText;
+let length = CardText.length - 1
+let indexes = getRandom(0,length)
+
+function getTexts(indexes){
+    const displayedText = []
+    for(let x = 0; x<10; x++){
+        displayedText[x] = CardText[(indexes[x])]
+        console.log(indexes[x])
+        // displayedText[x] = indexes[x]
+    }
+
+    return displayedText
+}
+
+function getRandom(min, max) {
+    const random = [];
+    for(let x = 0; x<10; x++){
+        random[x] = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return random;
+}
+
+export default getTexts(indexes);
